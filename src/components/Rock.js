de5a5561e803886f3b4cab2de982af18dba05209
louis frederic schefer <lf.schefer@gmail.com>
2023-06-05 13:54:30 +0200

    rpc finish

--- src/components/Rock.js
+++ src/components/Rock.js
@@ -1,5 +1,7 @@
 import * as React from "react"
 import "./Rock.css"
+import Confetti from 'react-confetti'
+
 
 export default function Rock(props) {
 
@@ -8,6 +10,7 @@ export default function Rock(props) {
   const [player, setPlayer] = React.useState()
   const [cpu, setCpu] = React.useState()
   const [gameScore, setGameScore] = React.useState({playerScore: 0, cpuScore: 0, draw: false})
+  const [gameFinish, setGameFinish] = React.useState({finish: false})
 
   const tirage = (x) => {
     const cpuPlay = Math.floor(Math.random() * pool.length)
@@ -83,17 +86,21 @@ export default function Rock(props) {
   }
 
   React.useEffect(() => {
-    console.log("watch")
     if (gameScore.playerScore >= 5) {
-      console.log("win")
+      setGameFinish( prev => {
+       return {...prev, finish: true, win: true}
+      })
     }
     if (gameScore.cpuScore >= 5) {
-      console.log("loose")
+      setGameFinish( prev => {
+        return {...prev, finish: true, win: false}
+       })
     }
   },[gameScore])
 
   const restScore =() => {
     setGameScore({playerScore: 0, cpuScore: 0, draw: false})
+    setGameFinish(false)
   }
 
   const roundText = gameScore.round ? "won-text" : "loose-text"
@@ -111,22 +118,29 @@ export default function Rock(props) {
 
   return (
     <main>
+      {gameFinish.win && <Confetti/>}
+      {gameFinish.finish ?
       <div className="rock-rules">
+        <h1>Rock | Paper | Cisor</h1>
+        <h1>Game Finish you {gameFinish.win ? "Win" : "Loose"} </h1>
+      </div>:
+
+      <><div className="rock-rules">
         <h1>Rock | Paper | Cisor</h1>
         <p>Please choose to start playing</p>
         <p>First to 5 win a game</p>
         <p>Round: <strong>You {gameScore.playerScore} | Cpu {gameScore.cpuScore}</strong></p>
-      </div>
-      <div className="result">
-        {displayScore}
-      </div>
+      </div><div className="result">
+          {displayScore}
+        </div></>
+      }
       <div className="rock-game">
         <div className="rock-btn">
           <button onClick={() => tirage("Rock")}>Rock</button>
           <button onClick={() => tirage("Paper")}>Paper</button>
           <button onClick={() => tirage("Cisor")}>Cisor</button>
         </div>
-        <button className="reset-btn" onClick={restScore}>Reset</button>
+        <button className="reset-btn" onClick={restScore}>{gameFinish.finish ? "Play again" : "Reset"}</button>
       </div>
     </main>
   )
