500662f671d12f3cb265146162b2f0aaa903a477
louis frederic schefer <lf.schefer@gmail.com>
2023-08-08 16:40:10 +0200

    taquin TODO

--- src/components/Taquin.js
+++ src/components/Taquin.js
@@ -3,7 +3,8 @@ import * as React from 'react'
 export default function Taquin(props) {
   return (
     <div className="taquin">
-      
+      <h1>Taquin</h1>
+      {/* TODO */}
     </div>
   )
 }
